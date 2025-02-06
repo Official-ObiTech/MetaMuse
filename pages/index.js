4e4ecf67b4ae344fa1267dc3b793821ed4bb3648
Obinna Justice <official.info.obitech@gmail.com>
2025-02-06 02:03:41 +0100

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -222,7 +222,7 @@ export default function Home() {
                     <FaInstagram />
                     <FaInstagram />
                     <FaInstagram />
-                    <FaInstagram />
+                  
                  
                    
                     
